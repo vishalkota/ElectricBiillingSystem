bc5725dfe549a44bb5b569f8995fb904fdc49f86
vishalkota <137258974+vishalkota@users.noreply.github.com>
2024-09-22 12:21:32 +0530

    Add files via upload

---
+++ EurekaRegistryService/src/main/java/com/eureka/demo/EurekaRegistryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryServiceApplication.class, args);
+	}
+
+}
